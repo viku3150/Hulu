ae8b23eb4d37267d9d219eadd56326179cd6b774
Vikash <21vikashgupta@gmail.com>
2021-12-10 13:24:06 +0530

    added firebase

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAFQ64M4_lHLpMxj5OfNdPy8WFGeY1YaFs",
+  authDomain: "hulu-clone-1f2f7.firebaseapp.com",
+  projectId: "hulu-clone-1f2f7",
+  storageBucket: "hulu-clone-1f2f7.appspot.com",
+  messagingSenderId: "977585169115",
+  appId: "1:977585169115:web:3d7c97983917d7237b22fe",
+};
